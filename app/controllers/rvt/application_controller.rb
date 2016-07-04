fe9309b9897c294201239af2cfad246bda393e3c
Genadi Samokovarov <gsamokovarov@gmail.com>
2016-07-04 09:48:41 +0300

    Prepare for 1.0.0 with Rails 5 support

--- app/controllers/rvt/application_controller.rb
+++ app/controllers/rvt/application_controller.rb
@@ -5,8 +5,27 @@ module RVT
     private
 
     def prevent_unauthorized_requests!
-      unless request.remote_ip.in?(RVT.config.whitelisted_ips)
+      remote_ip = GetSecureIp.new(request.env, RVT.config.whitelisted_ips).to_s
+
+      unless remote_ip.in?(RVT.config.whitelisted_ips)
         render nothing: true, status: :unauthorized
+      end
+    end
+
+    class GetSecureIp < ActionDispatch::RemoteIp::GetIp
+      def initialize(req, proxies)
+        # After rails/rails@07b2ff0 ActionDispatch::RemoteIp::GetIp initializes
+        # with a ActionDispatch::Request object instead of plain Rack
+        # environment hash. Keep both @req and @env here, so we don't if/else
+        # on Rails versions.
+        @req      = req
+        @env      = req.env
+        @check_ip = true
+        @proxies  = proxies
+      end
+
+      def filter_proxies(ips)
+        ips.reject { |ip| @proxies.include?(ip) }
       end
     end
   end
--- rvt.gemspec
+++ rvt.gemspec
@@ -15,12 +15,12 @@ Gem::Specification.new do |s|
   s.files      = Dir['{app,config,db,lib,vendor}/**/*', 'MIT-LICENSE', 'Rakefile', 'README.markdown']
   s.test_files = Dir['test/**/*']
 
-  rails_version = '~> 4.0'
+  rails_version = '>= 4.0'
 
-  s.add_dependency 'railties',          rails_version
-  s.add_dependency 'activemodel',       rails_version
-  s.add_dependency 'coffee-rails',      '~> 4.0'
-  s.add_dependency 'sprockets-rails',   '>= 2.0', '< 4.0'
+  s.add_dependency 'railties',    rails_version
+  s.add_dependency 'activemodel', rails_version
+  s.add_dependency 'coffee-rails'
+  s.add_dependency 'sprockets-rails'
 
   # We need those for the testing application to run.
   s.add_development_dependency 'actionmailer', rails_version
