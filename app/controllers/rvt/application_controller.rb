e3a1261f900488287e3c77a18bf5de1db3996a46
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-07-18 14:46:52 +0300

    Support Rails 5.2 CSRF by default configuration
    Fixes #3.

--- app/controllers/rvt/application_controller.rb
+++ app/controllers/rvt/application_controller.rb
@@ -1,5 +1,8 @@
 module RVT
   class ApplicationController < ActionController::Base
+    # Rails 5.2 has this by default. Skip it, as we don't need it for RVT.
+    skip_before_action :verify_authenticity_token, raise: false
+
     before_action :prevent_unauthorized_requests!
 
     private
